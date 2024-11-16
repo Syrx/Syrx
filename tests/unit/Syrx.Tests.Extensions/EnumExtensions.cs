f5c34ebeebed04f1dbf43ce98786343520d947d0
David Sexton <sextondjc@users.noreply.github.com>
2024-11-16 03:25:41 +0000

    Reapply test extensions project.

---
+++ tests/unit/Syrx.Tests.Extensions/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Syrx.Tests.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static IEnumerable<TResult> GetFlags<TResult>(Func<TResult, bool> filter = null) where TResult : Enum
+            => filter == null ?
+                Enum.GetValues(typeof(TResult)).Cast<TResult>() :
+                Enum.GetValues(typeof(TResult)).Cast<TResult>().Where(filter);
+    }
+}
---
+++ tests/unit/Syrx.Tests.Extensions/ExceptionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using static Xunit.Assert;
+
+namespace Syrx.Tests.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static void ArgumentNull(this ArgumentNullException exception, string parameter)
+        {
+            HasMessage(exception, $"Value cannot be null. (Parameter '{parameter}')");
+        }
+
+        public static void ArgumentOutOfRange(this ArgumentOutOfRangeException exception, string parameterName)
+        {
+            HasMessage(exception, $"Specified argument was out of the range of valid values. (Parameter '{parameterName}')");
+        }
+
+        public static void DuplicateKey(this ArgumentException exception, string key)
+        {
+            HasMessage(exception, $"An item with the same key has already been added. Key: {key}");
+        }
+
+        public static void HasMessage(this Exception exception, string message)
+        {
+            Equal(message.ReplaceLineEndings(), exception.Message.ReplaceLineEndings());
+        }
+
+        public static void DivideByZero(this Exception exception)
+        {
+            const string message = "Divide by zero error encountered.";
+            Equal(message, exception.Message);
+        }
+
+        public static void Print(this Exception excetion)
+        {
+            Console.WriteLine($@"{excetion.Message}");
+        }
+    }
+}
