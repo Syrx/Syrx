de48e08d45d061cc61ad0477ad22eaca4e929c6a
David Sexton <sextondjc@users.noreply.github.com>
2024-12-03 23:21:26 +0000

    Added more features to sync branches script

--- sync-branches.ps1
+++ sync-branches.ps1
@@ -3,9 +3,19 @@
 # author .. : @sextondjc
 # date .... : 2024.12.03
 # purpose . : creates new local branches across the different Syrx projects. 
-# usage ... : sync-branches.ps1 --new-branch 2.4.0 --remove-branch 2.3.0
+# usage ... : sync-branches.ps1 -branch 2.4.0 
 # -------------------------------------------------------------------------------------------------
-param ( [Parameter(Mandatory=$true)] [string]$branch )
+param ( 
+	[Parameter(Mandatory=$true)] 
+	[string]$branch,
+
+	[Parameter(Mandatory=$false)] 
+	[string]$pushToOrigin = "N",
+
+	[Parameter(Mandatory=$false)] 
+	[string]$promptOnLoop = "N"
+
+	)
 
 $repository_name = "Syrx.Npgsql"
 $repository_url = "https://github.com/Syrx/$repository_name"
@@ -60,11 +70,12 @@ foreach ($repo in $repositories) {
 		
     # Change to the repository directory
     Set-Location -Path $repository_root$repo
-
-	# switch to repo, check that this is what we want. 
-    $response = Prompt-User "This script will create and switch to new branch '$new_branch' for $repo. Do you want to continue?"
-    Handle-UserResponse -response $response 
-
+	
+	if($promptOnLoop -eq "Y"){
+		# switch to repo, check that this is what we want. 
+		$response = Prompt-User "This script will create and switch to new branch '$new_branch' for $repo. Do you want to continue?"
+		Handle-UserResponse -response $response 
+	}
 
     # Check the current status
     Write-Host "Checking current status..."
@@ -118,13 +129,21 @@ foreach ($repo in $repositories) {
 	Write-Host $git_result
     Evaluate-Result -errorcode $LASTEXITCODE -message "Failed to check local branch list."
 
-    # Push the new branch to the remote repository
-    $response = Prompt-User "Push new branch to remote?"
-	Handle-UserResponse -response $response 
-	Write-Host "Pushing new branch to remote..."
-    $git_result = git push -u origin $new_branch 2>&1 | Out-String
-    Evaluate-Result -errorcode $LASTEXITCODE -message "Failed to push to origin."
-
+	if($pushToOrigin -eq "Y") {
+		# Push the new branch to the remote repository
+		Write-Host "Pushing new branch to remote..."
+		$git_result = git push -u origin $new_branch 2>&1 | Out-String
+		Evaluate-Result -errorcode $LASTEXITCODE -message "Failed to push to origin."
+	}
+	
+	# Capture the branch list 
+	Write-Host "Capturing branch list..." 
+	$branchList = git branch -l 2>&1 | Out-String 
+	$branchInfo += [PSCustomObject]@{ 
+		Repository = $repo 
+		Branches = $branchList 
+	}
+	
     # Return to the original directory
     Set-Location -Path $PSScriptRoot
 	
@@ -133,4 +152,8 @@ foreach ($repo in $repositories) {
     Write-Host "-----------------------------------------------------------------------------"
 }
 
+# Print branch information as a table 
+$branchInfo | Format-Table -Property Repository, Branches -AutoSize
+
+Write-Host ""
 Write-Host "Done!"
